7a156e902cb87448c6fae54b1134563f6a2b6f62
Kiraogi <golokron3@yandex.ru>
2024-05-26 20:04:03 +0300

    Quick Sort + Merge Sort

---
+++ Sorting_Algorithms/Merge_Sort.py
@@ -0,0 +1,34 @@
+"""
+Сортировка слиянием (Merge Sort)
+Сортировка слиянием также является алгоритмом "разделяй и властвуй".
+Он разбивает массив на две половины, рекурсивно применяет сортировку слиянием к каждой половине, а затем сливает
+отсортированные половины в один отсортированный массив.
+"""
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+# Пример использования
+array = [3, 6, 8, 10, 1, 2, 1]
+sorted_array = merge_sort(array)
+print(sorted_array)
---
+++ Sorting_Algorithms/Quick_Sort.py
@@ -0,0 +1,21 @@
+"""
+Быстрая сортировка (Quick Sort)
+Быстрая сортировка — это эффективный алгоритм сортировки, использующий стратегию "разделяй и властвуй".
+Алгоритм выбирает элемент из массива, который называется опорным (pivot), и разделяет массив на две части:
+элементы меньше опорного и элементы больше или равные опорному.
+Затем он рекурсивно применяет ту же стратегию к обеим частям.
+"""
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+# Пример использования
+array = [3, 6, 8, 10, 1, 2, 1]
+sorted_array = quick_sort(array)
+print(sorted_array)
